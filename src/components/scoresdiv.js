e92dc442cf5d7801f8ace88ae3c5e5bd9fbe05af
nzguitarman <79890767+nzguitarman@users.noreply.github.com>
2022-03-05 14:33:04 +1300

    Fixes most layout issues
    Still requires names to be resolved, just seemed like a good time to commit

--- src/components/scoresdiv.js
+++ src/components/scoresdiv.js
@@ -15,26 +15,24 @@ export default function ScoresDiv() {
 
     let players = 7
 
-    const playersPercent = 80/players
-    const playersStylePercent = "" + playersPercent + "%"
     const [playerScores, setPlayerScores] = useState(Array(players).fill(0).map(row => new Array(7).fill(0)))
     const [playerTotals, setPlayerTotals] = useState(Array(players).fill(0))
 
-    const imgStyle = {
-        display: "block",
-        height: "100%",
-        
-    }
-
     const sumStyle = {
         textAlign: "center",
-        width: playersStylePercent,
         fontWeight: "bold",
-        display: "flex",
         justifyContent: "center",
-        alignItems: "center"
-        
+        alignItems: "center",
+        margin: "0 auto"   
     }
+
+    const bgRed ={  
+        
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat'
+      }
+
 
     function update(e){
         
@@ -46,20 +44,21 @@ export default function ScoresDiv() {
             copy[rowGet-1][colGet-1] = parseInt(e.target.value)
         }
         setPlayerScores(copy)
-        console.log(copy)
         
         
         // Update totals:
         let playerSum = 0
         let newTotals = Array(players).fill(0)
-        console.log(`new totals: ${newTotals}`)
         for (let i=0; i<players; i++) {
             playerSum = 0
             for (let j=0; j<8; j++) {
                 if(playerScores[i][j] > 0 && playerScores[i][j] != "") {
                     playerSum += playerScores[i][j]
+                } else if(playerScores[i][j] <= 0 && playerScores[i][j] != "") {
+                    playerSum += playerScores[i][j]
                 }
                 console.log(`reading: ${playerScores[i][j]}`)
+
             }
             newTotals[i] = playerSum
         }
@@ -71,7 +70,6 @@ export default function ScoresDiv() {
 
     function buildNames() {
         let playerCells = []
-        playerCells.push(<div style={{textAlign: "center", fontFamily: "sans-serif" }}>Names:</div>)
         for (let i = 1; i <= players; i++) {
             playerCells.push(<div className="nameLeft"><input type="text" style={{fontSize: 16, fontFamily: "sans-serif"}} /></div>)
         }  
@@ -79,10 +77,11 @@ export default function ScoresDiv() {
     }
     function buildCells(rowNum, color) {
         let cells = []
+        
         for (let i = 1; i <= players; i++) {
             let cellId = `cell${i}-row${rowNum}`
             let inputId = cellId + "-input"
-            cells.push(<div style={{width: playersStylePercent, backgroundColor: color}} key={cellId} id={cellId}><input type="number" id={inputId} onChange={update} style={{maxWidth: "100%"}} /></div>)
+            cells.push(<div className="innerDiv" style={{backgroundColor: color}} key={cellId} id={cellId}><input type="number" min="-100" id={inputId} onChange={update} style={{maxWidth: "100%"}} /></div>)
         }
         return cells
     }
@@ -90,7 +89,7 @@ export default function ScoresDiv() {
         let sumCells = []
         for (let i = 1; i <= players; i++) {
             let cellId = `cell${i}-row${rowNum}`
-            sumCells.push(<div style={sumStyle} className="finalScores" key={cellId} id={cellId}>{playerTotals[i-1]}</div>)
+            sumCells.push(<div style={sumStyle} className="finalScores innerDiv" key={cellId} id={cellId}>{playerTotals[i-1]}</div>)
         }
         return sumCells
     }
@@ -98,11 +97,13 @@ export default function ScoresDiv() {
     useEffect(() => {
         let playerSum = 0
         let newTotals = Array(players).fill(0)
-        console.log(`new totals: ${newTotals}`)
         for (let i=0; i<players; i++) {
             playerSum = 0
             for (let j=0; j<8; j++) {
+               
                 if(playerScores[i][j] > 0) {
+                    playerSum += playerScores[i][j]
+                }  else if(playerScores[i][j] < 0) {
                     playerSum += playerScores[i][j]
                 }
                 console.log(`reading: ${playerScores[i][j]}`)
@@ -115,38 +116,43 @@ export default function ScoresDiv() {
 
     return(
         <div id="wholePage">
-            <div className="topRow">{buildNames}</div>
+            <div className="board">
+                <div style={{textAlign: "center", fontFamily: "sans-serif", width: "17%", display: "flex", justifyContent: "center", fontSize: '1.5rem'}}>
+                    <p style={{margin: "auto 0"}}>Names:</p>
+                </div>
+                <div className='cellsContainer'>{buildNames}</div>
+            </div>
                     <div className="board">
-                        <div className="icons"><img className="stretchImg" src={red} style={imgStyle}/></div>
-                        {buildCells(1, "#ffd9d9")}          
+                        <div className="icons" style={{backgroundImage: "url(" + red + ")"}}></div>
+                        <div class="cellsContainer">{buildCells(1, "#ffd9d9")}</div>
                     </div>
                     <div className="board">
-                        <div className="icons"><img className="noStretch" src={coin} style={imgStyle}/></div>
-                        {buildCells(2, "#ffffff")}          
+                        <div className="iconsNoStretch" style={{display: "flex", justifyContent: "center"}}><img className="noStretch" src={coin}/></div>
+                        <div class="cellsContainer">{buildCells(2, "#ffffff")}</div>          
                     </div>
                     <div className="board">
-                        <div className="icons"><img className="noStretch" src={wonders} style={imgStyle}/></div>
-                        {buildCells(3, "#ffffff")}          
+                        <div className="iconsNoStretch" style={{display: "flex", justifyContent: "center"}}><img className="noStretch" src={wonders}/></div>
+                        <div class="cellsContainer">{buildCells(3, "#ffffff")}</div>          
                     </div>
                     <div className="board">
-                        <div className="icons"><img className="stretchImg" src={blue} style={imgStyle}/></div>
-                        {buildCells(4, "#d1d4ff")}          
+                    <div className="icons" style={{backgroundImage: "url(" + blue + ")"}}></div>
+                        <div class="cellsContainer">{buildCells(4, "#d1d4ff")}</div>          
                     </div>
                     <div className="board">
-                        <div className="icons"><img className="stretchImg" src={yellow} style={imgStyle}/></div>
-                        {buildCells(5, "#feffd1")}          
+                        <div className="icons" style={{backgroundImage: "url(" + yellow + ")"}}></div>
+                        <div class="cellsContainer">{buildCells(5, "#feffd1")}</div>          
                     </div>
                     <div className="board">
-                        <div className="icons"><img className="stretchImg" src={purple} style={imgStyle}/></div>
-                        {buildCells(6, "#fad1ff")}          
+                    <div className="icons" style={{backgroundImage: "url(" + purple + ")"}}></div>
+                        <div class="cellsContainer">{buildCells(6, "#fad1ff")}</div>          
                     </div>
                     <div className="board">
-                        <div className="icons"><img className="stretchImg" src={green} style={imgStyle}/></div>
-                        {buildCells(7, "#d1ffd6")}          
+                    <div className="icons" style={{backgroundImage: "url(" + green + ")"}}></div>
+                        <div class="cellsContainer">{buildCells(7, "#d1ffd6")}</div>          
                     </div>
                     <div className="board">
-                        <div className="icons"><img className="noStretch" src={sum} style={imgStyle}/></div>
-                        {buildSums(8)}          
+                        <div className="icons" style={{backgroundColor: "#000", display: "flex", justifyContent: "center"}}><img className="noStretch" src={sum}/></div>
+                        <div class="cellsContainer">{buildSums(8)}</div>          
                     </div>
         </div>
     )
